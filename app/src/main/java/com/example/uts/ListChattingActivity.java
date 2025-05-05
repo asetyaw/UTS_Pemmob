92e9f7eaa9b40bdc2c8693d98d2576b78c1667ec
asetyaw <aasetyawibawa@gmail.com>
2025-05-06 06:41:06 +0700

    penambahan

--- app/src/main/java/com/example/uts/ListChattingActivity.java
+++ app/src/main/java/com/example/uts/ListChattingActivity.java
@@ -12,7 +12,26 @@ public class ListChattingActivity extends AppCompatActivity {
             "Budi: Besok jadi ngumpul gak?",
             "Citra: Lihat tugas dong",
             "Deni: Woy online woy",
-            "Euis: Maaf telat bales 😅"
+            "Euis: Maaf telat bales 😅",
+            "Fahri: Ada yang punya catatan kuliah kemarin?",
+            "Gina: Guys, film baru udah keluar!",
+            "Hendra: Lagi di jalan nih, agak telat.",
+            "Intan: Semangat belajarnya ya semua!",
+            "Joko: Ada yang tau cara ngerjain soal ini?",
+            "Kartika: Otw!",
+            "Lina: Jangan lupa bawa makanan ya besok.",
+            "Miko: Siapa yang mau ikut main?",
+            "Nadia: Baru selesai meeting nih, capek banget.",
+            "Oscar: Ada info loker terbaru gak?",
+            "Putri: Lagi nyoba resep baru nih.",
+            "Rizky: Mantap!",
+            "Santi: Udah pada ngerjain proyek belum?",
+            "Toni: Besok jam berapa kumpulnya?",
+            "Vera: Seru banget acaranya!",
+            "Wawan: Ada yang bisa bantu coding?",
+            "Yeni: Lagi fokus ngerjain tugas akhir.",
+            "Zainal: Sampai jumpa besok!"
+
     };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
